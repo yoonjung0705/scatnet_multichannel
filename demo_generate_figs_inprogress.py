874ac4f1adcb3078a860c8d26df60e215901930a
Yoon Jung <yoonjung@mit.edu>
2019-10-18 02:27:24 -0400

    NF:demo_generate_figs_inprogress.py:drafted module for testing performance of trained network

--- demo_generate_figs_inprogress.py
+++ demo_generate_figs_inprogress.py
@@ -2,25 +2,42 @@
 # FIXME: for now we test prediction results for data simulated with hyperparameters that were used for the training data.
 # Later, include data simulated with hyperparameters whose values were not the ones used for the training data
 
+# FIXME: output results are 1 for k, 2 for diff ratios for all trajectories when tested for rnn. This is also for scat transformed trajectories
+# FIXME: check if there are ..meta..._test.pt files and if so, add number 0, 1, 2,... just like the meta file naming convention
+
 '''standard imports'''
 import os
+import numpy as np
+import re
 import torch
 import matplotlib.pyplot as plt
+from itertools import product
+from matplotlib.pyplot import cm
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.sampler import SequentialSampler
 
 '''custom libraries'''
 import scat_utils as scu
 import net_utils as nu
 
-# plt.style.use('dark_background')
-file_names = ['tbd_1_scat.pt', 'tbd_1.pt']
-file_names_meta = ['tbd_0_scat_meta_rnn_2.pt', 'tbd_0_meta_rnn_1.pt']
+file_names = ['tbd_1.pt', 'tbd_1_scat.pt']
+file_names_meta = ['tbd_0_meta_rnn_0.pt', 'tbd_0_scat_meta_rnn_0.pt']
 root_dir = './data/'
-epochs = [50, 40]
+epochs = [[50, 50], [50, 50]]
+
+# plt.style.use('dark_background')
+fontsize_title = 18
+fontsize_label = 14
+batch_size = 1000 # batch size when performing forward propagation on test data using trained weights
 
 '''sanity check'''
 # add .pt extension if not provided
 file_names = [os.path.splitext(file_name)[0] + '.pt' for file_name in file_names]
 file_names_meta = [os.path.splitext(file_name_meta)[0] + '.pt' for file_name_meta in file_names_meta]
+
+file_paths = [os.path.join(root_dir, file_name) for file_name in file_names]
+file_paths_meta = [os.path.join(root_dir, file_name_meta) for file_name_meta in file_names_meta]
+n_files = len(file_paths)
 
 # make file_names into list
 if isinstance(file_names, str):
@@ -30,8 +47,8 @@ if isinstance(file_names, str):
 assert(len(file_names) == len(file_names_meta))
 
 # check if all meta files are trained on the same dataset
-file_names_meta_common = [re.fullmatch(r'([a-z]{3}_[0-9]+)_.*_meta', file_name_meta).group(1) for file_name_meta in file_names_meta]
-assert(len(set(file_name_common)) == 1), "The given neural networks are trained on different data sets"
+file_names_meta_common = [re.fullmatch(r'([a-z]{3}_[0-9]+)_?.*_meta.*', file_name_meta).group(1) for file_name_meta in file_names_meta]
+assert(len(set(file_names_meta_common)) == 1), "The given neural networks are trained on different data sets"
 
 # check if all meta files are testing on the same dataset
 file_names_common = [re.fullmatch(r'([a-z]{3}_[0-9]+).*', file_name).group(1) for file_name in file_names]
@@ -39,56 +56,139 @@ assert(len(set(file_names_common)) == 1), "The given neural networks are tested 
 
 # if the training was done on scat transformed data, it should be tested on scat transformed data, too.
 # if the training was done on pure time series data, it should be tested on pure time series data, too.
-file_names_meta_is_scat = [re.fullmatch(r'[a-z]{3}_[0-9]+_?(.*)_?meta.*', file_name_meta).group(1) for file_name_meta in file_names_meta]
-file_names_is_scat = [re.fullmatch(r'[a-z]{3}_[0-9]+_?(.*)\.pt', file_name_meta).group(1) for file_name_meta in file_names_meta]
+file_names_meta_is_scat = ['scat' in file_name_meta for file_name_meta in file_names_meta]
+file_names_is_scat = ['scat' in file_name for file_name in file_names]
 assert(file_names_meta_is_scat == file_names_is_scat), "The training and test data are not transformed in the same manner"
 
-file_paths = [os.path.join(root_dir, file_name) for file_name in file_names]
-idx_epochs = [idx_epoch // 10 for idx_epoch in idx_epochs]
-
-for file_path in file_paths:
+for idx_file in range(n_files):
     #fig, ax = plt.subplots()
-    meta = torch.load(file_path)
+    file_name = file_names[idx_file]
+    file_path = file_paths[idx_file]
+    file_path_meta = file_paths_meta[idx_file]
+    file_name_meta = file_names_meta[idx_file]
+
+    samples = torch.load(file_path)
+    data = samples['data']
+    meta = torch.load(file_path_meta, map_location='cpu')
+
     n_labels = len(meta['label_names'])
-    if 'meta_rnn' in file_path:
+    epochs_file = epochs[idx_file]
+    idx_epochs = [epoch_file // 10 for epoch_file in epochs_file]
+    transformed = 'scat' in file_path
+    outputs = []
+    output_means = []
+    output_stds = []
+    labels_targets = []
+    if 'meta_rnn' in file_path_meta:
+        if transformed:
+            n_data_total = np.prod(data.shape[:-3])
+            n_features = np.prod(data.shape[-3:-1])
+            data_len = data.shape[-1]
+            input = data.reshape(data, [n_data_total, n_features, data_len])
+        else:
+            n_data_total = np.prod(data.shape[:-2])
+            n_features = data.shape[-2]
+            data_len = data.shape[-1]
+            input = data.reshape([n_data_total, n_features, data_len])
+        
+        input = torch.tensor(input, dtype=torch.get_default_dtype())
+        # following is shaped (n_labels, n_conditions)
+        labels = np.array(list(product(*samples['labels'])), dtype='float32').swapaxes(0, 1)
         for idx_label in range(n_labels):
-            net = nu.RNN(input_size=meta['input_size'], hidden_size=meta['hidden_size'], n_layers=meta['n_layers'], bidirectional=meta['bidirectional'])
-            net.load_state_dict(meta['weights'][idx_label][idx_epochs[idx_label]], map_location='cpu')
+            net = nu.RNN(input_size=meta['input_size'], hidden_size=meta['hidden_size'], output_size=1, n_layers=meta['n_layers'], bidirectional=meta['bidirectional'])
+            net.load_state_dict(meta['weights'][idx_label][idx_epochs[idx_label]])
+
+            dataset = nu.TimeSeriesDataset(input, np.zeros((n_data_total,)))
+            dataloader = DataLoader(dataset, batch_size=batch_size, sampler=SequentialSampler(range(n_data_total)))
+            output = []
+            print("forward propagation for {}".format(meta['label_names'][idx_label]))
+            for idx_batch, batch in enumerate(dataloader):
+                output.append(net(batch['data'].permute([2, 0, 1])).detach().numpy()[:, 0])
+                print("{} out of {} samples propagated".format(min((idx_batch + 1) * batch_size, n_data_total), n_data_total))
+            output = np.stack(output, axis=0)
+
+            if transformed:
+                output = output.reshape(samples['data'].shape[:-3])
+                output_mean = output.mean(axis=-1)
+                output_std = output.std(axis=-1)
+            else:
+                output = output.reshape(samples['data'].shape[:-2])
+                output_mean = output.mean(axis=-1)
+                output_std = output.std(axis=-1)
+            labels_target = labels[idx_label].reshape(output_mean.shape)
+            outputs.append(output)
+            output_means.append(output_mean)
+            output_stds.append(output_std)
+            labels_targets.append(labels_target)
+
+    else: # nn
+        if transformed:
+            n_data_total = np.prod(data.shape[:-3])
+            n_features = np.prod(data.shape[-3:-1])
+            data_len = data.shape[-1]
+            input = data.reshape([n_data_total, n_features * data_len])
+        else:
+            n_data_total = np.prod(data.shape[:-2])
+            n_features = data.shape[-2]
+            data_len = data.shape[-1]
+            input = data.reshape(data, [n_data_total, n_features * data_len])
+
+        input = torch.tensor(input, dtype=torch.get_default_dtype())
+        # following is shaped (n_labels, n_conditions)
+        labels = np.array(list(product(*samples['labels'])), dtype='float32').swapaxes(0, 1)
+        for idx_label in range(n_labels):
+            net = nu.Net(meta['n_nodes'])
+            net.load_state_dict(meta['weights'][idx_label][idx_epochs[idx_label]])
+
+            dataset = nu.TimeSeriesDataset(input, np.zeros((n_data_total,)))
+            dataloader = DataLoader(dataset, batch_size=batch_size, sampler=SequentialSampler(range(n_data_total)))
+            output = []
+            print("forward propagation for {}".format(meta['label_names'][idx_label]))
+            for idx_batch, batch in enumerate(dataloader):
+                output.append(net(batch['data']).detach().numpy()[:, 0])
+                print("{} out of {} samples propagated".format(min((idx_batch + 1) * batch_size, n_data_total), n_data_total))
+            output = np.stack(output, axis=0)
+
+            if transformed:
+                output = output.reshape(samples['data'].shape[:-3])
+                output_mean = output.mean(axis=-1)
+                output_std = output.std(axis=-1)
+            else:
+                output = output.reshape(samples['data'].shape[:-2])
+                output_mean = output.mean(axis=-1)
+                output_std = output.std(axis=-1)
+            labels_target = labels[idx_label].reshape(output_mean.shape)
+            outputs.append(output)
+            output_means.append(output_mean)
+            output_stds.append(output_std)
+            labels_targets.append(labels_target)
+
+    result = {'labels':samples['labels'], 'label_names':samples['label_names'], 'prediction':outputs, 'epochs':epochs_file, 'file_name':file_name, 'root_dir':root_dir}
+    torch.save(result, os.path.join(root_dir, os.path.splitext(file_name_meta)[0] + '_test.pt'))
+            
+    if n_labels == 1:
+        fig, ax = plt.subplots()
+        ax.errorbar(labels_target, output_mean, yerr=output_std, ls='', capsize=10, fmt='o')
+        ax.errorbar(labels_target, labels_target, ls='--')
+        ax.set_title(label_names[0].replace('_', ' ').capitalize(), fontsize=fontsize_title)
+        ax.set_xlabel('Ground truth', fontsize=fontsize_label)
+        ax.set_ylabel('Prediction', fontsize=fontsize_label)
+        plt.show()
+    elif n_labels == 2:
+        n_conditions = len(labels_target)
+        color = cm.rainbow(np.linspace(0, 1, n_conditions))
+        fig, ax = plt.subplots()
+        for i in range(n_conditions):
+            ax.errorbar(output_means[0][i], output_means[1][i], xerr=output_stds[0][i], yerr=output_stds[1][i], fmt='o', capsize=10, c=color[i])
+            ax.plot([output_means[0][i], labels_targets[0][i]], [output_means[1][i], labels_targets[1][i]], marker='o', color=color[i])
+
+        ax.set_xlabel(samples['label_names'][0], fontsize=fontsize_label)
+        ax.set_ylabel(samples['label_names'][1], fontsize=fontsize_label)
+        plt.show()
 
 
 
-
-
-
-
-
-
-
-    elif 'meta_nn' in file_path:
-    else:
-        raise IOError("Invalid file name: should be meta data of the trained network")
-
-
-
-    torch.load(
-
-
-output_arr = output.data.numpy()
-target_arr = output.data.numpy()
-output_arr = np.squeeze(output_arr, axis=1)
-target_arr = np.squeeze(target_arr, axis=1)
-output_arr1 = output_arr[:1000]
-output_arr2 = output_arr[1000:2000]
-output_arr3 = output_arr[2000:3000]
-output_arr4 = output_arr[3000:]
-std1 = output_arr1.std()
-std2 = output_arr2.std()
-std3 = output_arr3.std()
-std4 = output_arr4.std()
-mean1 = output_arr1.mean()
-mean2 = output_arr2.mean()
-mean3 = output_arr3.mean()
-mean4 = output_arr4.mean()
+"""
 ax.errorbar([1,2,3,4],[mean1, mean2, mean3, mean4], yerr=[std1, std2, std3, std4], ls='', capsize=10, fmt='o')
 ax.errorbar([1,2,3,4],[1,2,3,4], ls='--')
 ax.set_title('Diffusion coefficients',fontsize=18)
@@ -97,38 +197,6 @@ ax.set_ylabel('Prediction', fontsize=18)
 ax.set_xlim([0.8,4.2])
 ax.set_ylim([0.8,4.2])
 plt.show()
-
-
-
-
-
-
-fig, ax = plt.subplots()
-ax.plot(loss_arr_k, label='Training error')
-ax.plot(val_arr_k, label='Test error')
-ax.set_title('Inferring spring constant ratio', fontsize=18)
-ax.set_ylabel('Mean squared error', fontsize=18)
-ax.set_xlabel('Epochs', fontsize=18)
-ax.legend()
-
-fig2, ax2 = plt.subplots()
-ax2.plot(loss_arr_T, label='Training error')
-ax2.plot(val_arr_T, label='Test error')
-ax2.set_title('Inferring temperature ratio', fontsize=18)
-ax2.set_ylabel('Mean squared error', fontsize=18)
-ax2.set_xlabel('Epochs', fontsize=18)
-ax2.legend()
-
-
-
-
-
-
-
-
-
-
-
 
 validation_k_arr = np.squeeze(validation_k.data.numpy(), axis=1)
 validation_T_arr = np.squeeze(validation_T.data.numpy(), axis=1)
@@ -172,7 +240,7 @@ ax.set_ylabel('Temperature ratio', fontsize=18)
 ax.set_xlabel('Spring constant ratio', fontsize=18)
 
 plt.show()
-
+"""
 
 
 
