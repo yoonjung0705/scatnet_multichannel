724946f13b305ca6f95dacb0303df004b47d22f9
Yoon Jung <yoonjung@mit.edu>
2020-02-23 15:05:46 -0500

    REF:commit before creating branch

--- train_rnn.py
+++ train_rnn.py
@@ -20,26 +20,8 @@ from apex import amp
 import common_utils as cu
 import scat_utils as scu
 
-ROOT_DIR = './data/'
-
-OOT_DIR = './data/simulations/two_beads/'
-
-'''common inputs'''
-root_dir = ROOT_DIR
-file_names_data = ['tbd_0.pt', 'tbd_1.pt']
-#file_names_scat = []
-
-n_epochs_max = 2000
-train_ratio = 0.8
-batch_size = 64
-n_workers = 4
-
-# RNN inputs
-hidden_sizes = [20, 50, 100]
-n_layerss = [2, 3]
-bidirectionals = [True]
-lr=0.001
-betas=(0.9, 0.999)
+ROOT_DIR = './data/simulations/two_beads/'
+file_name = 'tbd_0.pt'
 
 # Training settings
 parser = argparse.ArgumentParser(description='RNN training') # FIXME: change description, read up on argparse
@@ -70,111 +52,83 @@ parser.add_argument('--seed', type=int, default=42, metavar='S',
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='how many batches to wait before logging training status (default: 10)')
 
-def train_rnn(file_name, hidden_size, n_layers=1, bidirectional=False, classifier=False, n_epochs_max=2000,
-        train_ratio=0.8, batch_size=100, n_workers=4, root_dir=ROOT_DIR, lr=0.001, betas=(0.9, 0.999)):
-    '''
-    trains the recurrent neural network given a file that contains data.
-    this data can be either scat transformed or pure simulated data
+'''START FIXING FROM HERE'''
+file_path = os.path.join(root_dir, file_name + '.pt')
+#transformed = 'scat' in file_name
+samples = torch.load(file_path)
+nums = cu.match_filename(r'{}_meta_rnn_([0-9]+).pt'.format(file_name), root_dir=root_dir)
+nums = [int(num) for num in nums]; idx = max(nums) + 1 if nums else 0
+file_name_meta = '{}_meta_rnn_{}.pt'.format(file_name, idx)
+# data shape: (n_param_1, n_param_2,..., n_param_N, n_samples_total, n_channels, (n_nodes), data_len)
+data, labels, label_names = samples['data'], samples['labels'], samples['label_names']
+# the number of dimensions that do not correspond to the batch dimension is 4 if scat transformed.
+# Otherwise, it's 3
+n_none_param_dims = 4 if transformed else 3
+n_samples_total = data.shape[-n_none_param_dims]
+n_data_total = np.prod(data.shape[:-(n_none_param_dims - 1)])
+n_labels = len(label_names) # number of labels to predict
+if classifier:
+    assert(isinstance(hidden_size, int)), "Invalid format of hidden_size given. Should be type int"
+else:
+    if n_labels == 1 and isinstance(hidden_size, int): hidden_size = [hidden_size]
+    assert(len(hidden_size) == n_labels), "Invalid format of hidden state sizes given.\
+        Should have length n_labels"
+    assert(all([isinstance(hidden_size_label, int) for hidden_size_label in hidden_size])),\
+        "Invalid format of hidden_size given. Should be list with int type elements"
 
-    inputs
-    ------
-    file_name: string type name of file
-    hidden_size: list type, sizes of hidden states
-    n_layers: number of recurrent layers
-    bidirectional: if True, becomes a bidirectional LSTM
-    classifier: boolean indicating whether it's a classifier or regressor.
-    n_epochs_max: maximum number of epochs to run. 
-        can terminate with ctrl + c to move on to next neural network training.
-    train_ratio: float indicating ratio for training data. should be between 0 and 1
-    batch_size: size of batch for computing gradient
-    n_workers: how many subprocesses to use for data loading.
-        0 means that the data will be loaded in the main process.
-    root_dir: string type root directory name
-    lr - float type learning rate
-    betas - tuple of floats indicating betas arguments in Adam optimizer
+index = nu._train_test_split(n_data_total, train_ratio); index['val'] = index.pop('test')
 
-    outputs
-    -------
-    None: saves weights and meta data into file
-    '''
-    file_name, _ = os.path.splitext(file_name)
-    file_path = os.path.join(root_dir, file_name + '.pt')
-    transformed = 'scat' in file_name
-    samples = torch.load(file_path)
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    nums = cu.match_filename(r'{}_meta_rnn_([0-9]+).pt'.format(file_name), root_dir=root_dir)
-    nums = [int(num) for num in nums]; idx = max(nums) + 1 if nums else 0
-    file_name_meta = '{}_meta_rnn_{}.pt'.format(file_name, idx)
-    # data shape: (n_param_1, n_param_2,..., n_param_N, n_samples_total, n_channels, (n_nodes), data_len)
-    data, labels, label_names = samples['data'], samples['labels'], samples['label_names']
-    # the number of dimensions that do not correspond to the batch dimension is 4 if scat transformed.
-    # Otherwise, it's 3
-    n_none_param_dims = 4 if transformed else 3
-    n_samples_total = data.shape[-n_none_param_dims]
-    n_data_total = np.prod(data.shape[:-(n_none_param_dims - 1)])
-    n_labels = len(label_names) # number of labels to predict
-    if classifier:
-        assert(isinstance(hidden_size, int)), "Invalid format of hidden_size given. Should be type int"
-    else:
-        if n_labels == 1 and isinstance(hidden_size, int): hidden_size = [hidden_size]
-        assert(len(hidden_size) == n_labels), "Invalid format of hidden state sizes given.\
-            Should have length n_labels"
-        assert(all([isinstance(hidden_size_label, int) for hidden_size_label in hidden_size])),\
-            "Invalid format of hidden_size given. Should be list with int type elements"
+# reshape data. output is shaped (n_data_total, n_channels * (n_scat_nodes), data_len).
+# (n_scat_nodes) means 1 if data not transformed
+data = np.reshape(data, (n_data_total, -1, data.shape[-1]))
+input_size = data.shape[-2]
 
-    index = nu._train_test_split(n_data_total, train_ratio); index['val'] = index.pop('test')
+# initialize meta data and save it to a file
+meta = {'file_name':file_name_meta, 'root_dir':root_dir, 'input_size':input_size,
+    'hidden_size':hidden_size, 'n_layers':n_layers, 'bidirectional':bidirectional,
+    'classifier':classifier, 'n_epochs_max':n_epochs_max, 'train_ratio':train_ratio,
+    'batch_size':batch_size, 'n_workers':n_workers, 'index':index,
+    'labels':samples['labels'], 'label_names':samples['label_names']}
 
-    # reshape data. output is shaped (n_data_total, n_channels * (n_scat_nodes), data_len).
-    # (n_scat_nodes) means 1 if data not transformed
-    data = np.reshape(data, (n_data_total, -1, data.shape[-1]))
-    input_size = data.shape[-2]
+labels = np.array(list(product(*labels)), dtype='float32') # shaped (n_conditions, n_labels)
+if classifier:
+    label_to_idx = {tuple(condition):idx_condition for idx_condition, condition in enumerate(labels)}
+    n_conditions = len(label_to_idx)
 
-    # initialize meta data and save it to a file
-    meta = {'file_name':file_name_meta, 'root_dir':root_dir, 'input_size':input_size,
-        'hidden_size':hidden_size, 'n_layers':n_layers, 'bidirectional':bidirectional,
-        'classifier':classifier, 'n_epochs_max':n_epochs_max, 'train_ratio':train_ratio,
-        'batch_size':batch_size, 'n_workers':n_workers, 'index':index, 'device':device,
-        'labels':samples['labels'], 'label_names':samples['label_names']}
+    meta.update({'epoch':[], 'weights':None, 'elapsed':[], 'loss':{'train':[], 'val':[]},
+        'criterion':'cross_entropy_sum', 'label_to_idx':label_to_idx})
+    nu._init_meta(**meta)
 
-    labels = np.array(list(product(*labels)), dtype='float32') # shaped (n_conditions, n_labels)
-    if classifier:
-        label_to_idx = {tuple(condition):idx_condition for idx_condition, condition in enumerate(labels)}
-        n_conditions = len(label_to_idx)
+    labels = np.arange(n_conditions) # shaped (n_conditions,)
+    labels = np.repeat(labels, n_samples_total) # shaped (n_conditions * n_samples_total,)
+    # which, for example, looks like [0,0,0,1,1,1,2,2,2,3,3,3,4,4,4] 
+    # for n_samples_total being 3 and n_conditions being 5
 
-        meta.update({'epoch':[], 'weights':None, 'elapsed':[], 'loss':{'train':[], 'val':[]},
-            'criterion':'cross_entropy_sum', 'label_to_idx':label_to_idx})
-        nu._init_meta(**meta)
-
-        labels = np.arange(n_conditions) # shaped (n_conditions,)
-        labels = np.repeat(labels, n_samples_total) # shaped (n_conditions * n_samples_total,)
-        # which, for example, looks like [0,0,0,1,1,1,2,2,2,3,3,3,4,4,4] 
-        # for n_samples_total being 3 and n_conditions being 5
-
-        dataset = nu.TimeSeriesDataset(data, labels, transform=nu.ToTensor())
-        # train the neural network for classification
-        print("Beginning training of {}:".format(', '.join(samples['label_names'])))
-        _train_rnn(dataset, index, hidden_size=hidden_size, n_layers=n_layers,
+    dataset = nu.TimeSeriesDataset(data, labels, transform=nu.ToTensor())
+    # train the neural network for classification
+    print("Beginning training of {}:".format(', '.join(samples['label_names'])))
+    _train_rnn(dataset, index, hidden_size=hidden_size, n_layers=n_layers,
+        bidirectional=bidirectional, classifier=classifier, n_epochs_max=n_epochs_max,
+        batch_size=batch_size, n_workers=n_workers, device=device,
+        file_name=file_name_meta, root_dir=root_dir, lr=lr, betas=betas)
+else:
+    meta.update({'epoch':[[] for _ in range(n_labels)], 'weights':[None for _ in range(n_labels)],
+        'elapsed':[[] for _ in range(n_labels)], 'loss':[{'train':[], 'val':[]}
+            for _ in range(n_labels)], 'criterion':'rmse'})
+    nu._init_meta(**meta)
+    
+    # following is shaped (n_labels, n_conditions)
+    labels = labels.swapaxes(0, 1)
+    # following is shaped (n_labels, n_data_total)
+    labels = np.tile(labels[:, :, np.newaxis], [1, 1, n_samples_total]).reshape([n_labels, n_data_total])
+    for idx_label in range(n_labels):
+        dataset = nu.TimeSeriesDataset(data, labels[idx_label], transform=nu.ToTensor())
+        # train the rnn for the given idx_label
+        print("Beginning training of {}:".format(samples['label_names'][idx_label]))
+        _train_rnn(dataset, index, hidden_size=hidden_size[idx_label], n_layers=n_layers,
             bidirectional=bidirectional, classifier=classifier, n_epochs_max=n_epochs_max,
-            batch_size=batch_size, n_workers=n_workers, device=device,
+            batch_size=batch_size, n_workers=n_workers, device=device, idx_label=idx_label,
             file_name=file_name_meta, root_dir=root_dir, lr=lr, betas=betas)
-    else:
-        meta.update({'epoch':[[] for _ in range(n_labels)], 'weights':[None for _ in range(n_labels)],
-            'elapsed':[[] for _ in range(n_labels)], 'loss':[{'train':[], 'val':[]}
-                for _ in range(n_labels)], 'criterion':'rmse'})
-        nu._init_meta(**meta)
-        
-        # following is shaped (n_labels, n_conditions)
-        labels = labels.swapaxes(0, 1)
-        # following is shaped (n_labels, n_data_total)
-        labels = np.tile(labels[:, :, np.newaxis], [1, 1, n_samples_total]).reshape([n_labels, n_data_total])
-        for idx_label in range(n_labels):
-            dataset = nu.TimeSeriesDataset(data, labels[idx_label], transform=nu.ToTensor())
-            # train the rnn for the given idx_label
-            print("Beginning training of {}:".format(samples['label_names'][idx_label]))
-            _train_rnn(dataset, index, hidden_size=hidden_size[idx_label], n_layers=n_layers,
-                bidirectional=bidirectional, classifier=classifier, n_epochs_max=n_epochs_max,
-                batch_size=batch_size, n_workers=n_workers, device=device, idx_label=idx_label,
-                file_name=file_name_meta, root_dir=root_dir, lr=lr, betas=betas)
 
 def _train_rnn(dataset, index, hidden_size, n_layers, bidirectional, classifier, n_epochs_max, batch_size,
         device, n_workers, file_name, root_dir=ROOT_DIR, idx_label=None, lr=0.001, betas=(0.9, 0.999)):
