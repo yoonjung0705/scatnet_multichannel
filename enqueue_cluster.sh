4bec0e0abf0f7cb2527d1f5135a80b467f281698
Yoon Jung <yoonjung0705@gmail.com>
2020-03-03 11:49:36 -0500

    NF:using exit.log which contains the jobid osf all the jobs that exited with failure

--- enqueue_cluster.sh
+++ enqueue_cluster.sh
@@ -2,7 +2,7 @@
 # submits jobs to the cluster using list of hyperparameters in the params.csv file. In detail, it does the following:
 # - updates the status of the jobs by removing lines that were successfully completed (status DONE).
 # - go through line by line, submit job if it’s not a failed job until the number of submitted jobs becomes N_JOBS_MAX_NORMAL. update the job id and submission count
-# - go through line by line, submit job if it’s a failed job until the number of submitted jobs becomes N_JOBS_MAX_FAILED. update the job id and submission count
+# - go through line by line, submit job if it’s a failed job until the number of submitted jobs becomes N_JOBS_MAX_EXIT. update the job id and submission count
 # 
 # two queues exist: the cluster queue and the parameters queue. 
 # for job submission to the cluster I term it enqueue for the cluster, 
@@ -14,42 +14,62 @@ cd ${SCATNET_DIR}
 
 # job count
 N_JOBS_MAX_NORMAL=4
-N_JOBS_MAX_FAILED=1
+N_JOBS_MAX_EXIT=1
 SUBMIT_COUNT_MAX=3
-BATCH_SIZE_FAILED=32 # use a smaller batch size for previously failed jobs
+BATCH_SIZE_EXIT=32 # use a smaller batch size for previously failed jobs
+FILE_NAME_PARAMS="params.csv"
+FILE_NAME_EXIT="exit.log"
+FILE_NAME_JOB="rnn.lsf"
+FILE_NAME_JOB_TEMPLATE="rnn_template.lsf"
 
-# get jobids. output example: 41422,41423,
-JOBID_DONE=$(bjobs -a 2> /dev/null | grep "DONE" | cut -d' ' -f1 | awk 'BEGIN { ORS = "," } { print }')
+# get done jobids. output example: 41422,41423,
+JOBIDS_DONE=$(bjobs -a 2> /dev/null | grep "DONE" | cut -d' ' -f1 | awk 'BEGIN { ORS = "," } { print }')
+# get exit jobids. output example: 41422\n41423
+JOBIDS_EXIT_RECENT=$(bjobs -a 2> /dev/null | grep "EXIT" | cut -d' ' -f1
 # TODO: 
 # - get the exit jobids from bjobs -a - (A)
-# - get the exit jobids from fail.log - (B)
-# - for each jobid in (A), see if it's in (B) and if not, append to (B)
+# - get the exit jobids from exit.log - (B)
 # - for each jobid in jobid_done, see if it's in (B) and if so, remove it from (B)
-# - the JOB_EXIT_REGEX should be defined from the fail.log file
+# - for each jobid in (A), see if it's in (B) and if not, append to (B)
+# - the JOBIDS_EXIT_REGEX should be defined from the exit.log file
 # - you don't have to define job exit regex with the "|" thing.
-# just check with grep directly: grep "$JOBID" -qw "fail.log"
+# just check with grep directly: grep "$JOBID" -qw "exit.log"
 
-#JOBID_EXIT=$(bjobs -a 2> /dev/null | grep "EXIT" | cut -d' ' -f1 | awk 'BEGIN { ORS = "," } { print }')
-#JOBID_EXIT_CUM=$(cat fail.log | paste -sd,)
+#JOBIDS_EXIT=$(bjobs -a 2> /dev/null | grep "EXIT" | cut -d' ' -f1 | awk 'BEGIN { ORS = "," } { print }')
+#JOBIDS_EXIT_CUM=$(cat exit.log | paste -sd,)
 
 # the "No unfinished jobs" is an error message and therefore does not count in wc -l
 N_JOBS_SUBMITTED=$(expr $(bjobs 2>/dev/null | wc -l) - 1)
 N_JOBS_SUBMITTED=$(( N_JOBS_SUBMITTED > 0 ? N_JOBS_SUBMITTED : 0 ))
-FILE_NAME_PARAMS="params.csv"
 
 # get regular expression for grep. 
 # output example for jobs done: ^41422|^41423
 # output example for jobs exit: 41422|41423
-JOB_DONE_REGEX="$(echo $JOBID_DONE | rev | cut -c 2- | rev | sed 's/,/|^/g')"
-JOB_EXIT_REGEX="$(echo $JOBID_EXIT | rev | cut -c 2- | rev | sed 's/,/|/g')"
+JOBIDS_DONE_REGEX="$(echo $JOBIDS_DONE | rev | cut -c 2- | rev | sed 's/,/|^/g')"
+#JOBIDS_EXIT_REGEX="$(echo $JOBIDS_EXIT | rev | cut -c 2- | rev | sed 's/,/|/g')"
 
-if [ ! -z $JOB_DONE_REGEX ]
+# remove the finished jobs in the params.csv file 
+if [ ! -z $JOBIDS_DONE_REGEX ]
 then
-    grep -vE "^$JOB_DONE_REGEX" ${FILE_NAME_PARAMS} > tmp_file
+    grep -vE "^$JOBIDS_DONE_REGEX" ${FILE_NAME_PARAMS} > tmp_file
     mv tmp_file ${FILE_NAME_PARAMS}
 fi
 
+# remove the finished jobs in the exit.log file 
+if [ ! -z $JOBIDS_DONE_REGEX ]
+then
+    grep -vE "^$JOBIDS_DONE_REGEX" ${FILE_NAME_EXIT} > tmp_file
+    mv tmp_file ${FILE_NAME_EXIT}
+fi
 
+# add failed jobs in the exit.log file
+for JOBID_EXIT_RECENT in $JOBIDS_EXIT_RECENT
+do
+    if $(! grep -qw $JOBID_EXIT_RECENT ${FILE_NAME_EXIT})
+    then
+        echo $JOBID_EXIT_RECENT >> ${FILE_NAME_EXIT}
+    done
+done
 
 # initialize line count variable for submitting jobs that have never entered the cluster
 LINE_COUNT=0
@@ -79,7 +99,7 @@ cat ${FILE_NAME_PARAMS} | while [[ "${N_JOBS_SUBMITTED}" -lt "${N_JOBS_MAX_NORMA
         if [[ $SUBMIT_COUNT -eq 0 ]]
         then
             # text substitution using the template
-            cat rnn_template.lsf | sed \
+            cat ${FILE_NAME_JOB_TEMPLATE} | sed \
                 -e "s/<FILE_NAME>/${FILE_NAME}/g" \
                 -e "s|<ROOT_DIR>|${ROOT_DIR}|g" \
                 -e "s/<HIDDEN_SIZE>/${HIDDEN_SIZE}/g" \
@@ -96,10 +116,10 @@ cat ${FILE_NAME_PARAMS} | while [[ "${N_JOBS_SUBMITTED}" -lt "${N_JOBS_MAX_NORMA
                 -e "s/<OPT_LEVEL>/${OPT_LEVEL}/g" \
                 -e "s/<SEED>/${SEED}/g" \
                 -e "s/<LOG_INTERVAL>/${LOG_INTERVAL}/g" \
-                > rnn.lsf
+                > ${FILE_NAME_JOB}
 
             # submit the job and get new job id
-            JOBID_NEW=$(bsub < rnn.lsf | grep -o "[0-9]\+")
+            JOBID_NEW=$(bsub < ${FILE_NAME_JOB} | grep -o "[0-9]\+")
             SUBMIT_COUNT_NEW=`expr ${SUBMIT_COUNT} + 1`
             # update the jobid and the submission count in the params.csv file
             awk 'BEGIN { FS=","; OFS="," } NR==line_count { $1=jobid_new; $2=submit_count_new };1' \
@@ -115,7 +135,7 @@ N_JOBS_SUBMITTED=$(expr $(bjobs 2>/dev/null | wc -l) - 1)
 N_JOBS_SUBMITTED=$(( N_JOBS_SUBMITTED > 0 ? N_JOBS_SUBMITTED : 0 ))
 
 echo "N_JOBS_SUBMITTED: $N_JOBS_SUBMITTED"
-echo "N_JOBS_MAX_FAILED: $N_JOBS_MAX_FAILED"
+echo "N_JOBS_MAX_EXIT: $N_JOBS_MAX_EXIT"
 echo "checking [ -s FILE_NAME_PARAMS ]"
 [ -s ${FILE_NAME_PARAMS} ]
 echo $?
@@ -124,7 +144,7 @@ LINE_COUNT=0
 # read parameters 1 line at a time. If the number of times the job has been submitted is more than 1, 
 # and if the job is a recently failed one, submit the job and get the id
 # then, replace the jobid and submit count and update the file
-cat ${FILE_NAME_PARAMS} | while [[ "${N_JOBS_SUBMITTED}" -lt "${N_JOBS_MAX_FAILED}" ]] && [ -s ${FILE_NAME_PARAMS} ] && IFS=, read \
+cat ${FILE_NAME_PARAMS} | while [[ "${N_JOBS_SUBMITTED}" -lt "${N_JOBS_MAX_EXIT}" ]] && [ -s ${FILE_NAME_PARAMS} ] && IFS=, read \
         JOBID \
         SUBMIT_COUNT \
         FILE_NAME \
@@ -148,11 +168,11 @@ cat ${FILE_NAME_PARAMS} | while [[ "${N_JOBS_SUBMITTED}" -lt "${N_JOBS_MAX_FAILE
 
         echo "LINE_COUNT:$LINE_COUNT"
         echo "JOBID:$JOBID"
-        echo "JOB_EXIT_REGEX:$JOB_EXIT_REGEX"
+        echo "JOBIDS_EXIT_REGEX:$JOBIDS_EXIT_REGEX"
         echo "SUBMIT_COUNT:$SUBMIT_COUNT"
         echo "SUBMIT_COUNT_MAX:$SUBMIT_COUNT_MAX"
-        echo "checking echo JOBID | grep -qEw JOB_EXIT_REGEX"
-        $(echo "$JOBID" | grep -qEw "$JOB_EXIT_REGEX")
+        echo "checking echo JOBID | grep -qEw JOBIDS_EXIT_REGEX"
+        $(echo "$JOBID" | grep -qEw "$JOBIDS_EXIT_REGEX")
         echo $?
         echo "checking [[ SUBMIT_COUNT -lt SUBMIT_COUNT_MAX ]]"
         [[ $SUBMIT_COUNT -lt $SUBMIT_COUNT_MAX ]]
@@ -161,10 +181,10 @@ cat ${FILE_NAME_PARAMS} | while [[ "${N_JOBS_SUBMITTED}" -lt "${N_JOBS_MAX_FAILE
         [[ $SUBMIT_COUNT -ge 1 ]]
         echo $?
 
-        if $(echo "$JOBID" | grep -qEw "$JOB_EXIT_REGEX") && [[ $SUBMIT_COUNT -lt $SUBMIT_COUNT_MAX ]] && [[ $SUBMIT_COUNT -ge 1 ]]
+        if $(echo "$JOBID" | grep -qw ${FILE_NAME_EXIT}) && [[ $SUBMIT_COUNT -lt $SUBMIT_COUNT_MAX ]] && [[ $SUBMIT_COUNT -ge 1 ]]
         then
             # text substitution using the template
-            cat rnn_template.lsf | sed \
+            cat ${FILE_NAME_JOB_TEMPLATE} | sed \
                 -e "s/<FILE_NAME>/${FILE_NAME}/g" \
                 -e "s|<ROOT_DIR>|${ROOT_DIR}|g" \
                 -e "s/<HIDDEN_SIZE>/${HIDDEN_SIZE}/g" \
@@ -174,17 +194,17 @@ cat ${FILE_NAME_PARAMS} | while [[ "${N_JOBS_SUBMITTED}" -lt "${N_JOBS_MAX_FAILE
                 -e "s/<IDX_LABEL>/${IDX_LABEL}/g" \
                 -e "s/<EPOCHS>/${EPOCHS}/g" \
                 -e "s/<TRAIN_RATIO>/${TRAIN_RATIO}/g" \
-                -e "s/<BATCH_SIZE>/${BATCH_SIZE_FAILED}/g" \
+                -e "s/<BATCH_SIZE>/${BATCH_SIZE_EXIT}/g" \
                 -e "s/<N_WORKERS>/${N_WORKERS}/g" \
                 -e "s/<LR>/${LR}/g" \
                 -e "s/<BETAS>/${BETAS}/g" \
                 -e "s/<OPT_LEVEL>/${OPT_LEVEL}/g" \
                 -e "s/<SEED>/${SEED}/g" \
                 -e "s/<LOG_INTERVAL>/${LOG_INTERVAL}/g" \
-                > rnn.lsf
+                > ${FILE_NAME_JOB}
 
             # submit the job and get new job id
-            JOBID_NEW=$(bsub < rnn.lsf | grep -o "[0-9]\+")
+            JOBID_NEW=$(bsub < ${FILE_NAME_JOB} | grep -o "[0-9]\+")
             SUBMIT_COUNT_NEW=`expr ${SUBMIT_COUNT} + 1`
             # update the jobid and the submission count in the params.csv file
             awk 'BEGIN { FS=","; OFS="," } NR==line_count { $1=jobid_new; $2=submit_count_new };1' \
