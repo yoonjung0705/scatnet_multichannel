ceab9b7f604bbb0833074d9b504a7919ac973d43
Yoon Jung <yoonjung@mit.edu>
2020-03-23 02:54:21 -0400

    DOC

--- enqueue_cluster.sh
+++ enqueue_cluster.sh
@@ -18,10 +18,10 @@ SCATNET_DIR="/nobackup/users/yoonjung/repos/scatnet_multichannel"
 cd ${SCATNET_DIR}
 
 # job count
-N_JOBS_MAX_NORMAL=5 # max number of jobs allowed to run simutaneously for new jobs. do not go beyond 8
+N_JOBS_MAX_NORMAL=4 # max number of jobs allowed to run simutaneously for new jobs. do not go beyond 8
 N_JOBS_MAX_EXIT=3 # max number of jobs allowed to run simutaneously for previously failed jobs
 SUBMIT_COUNT_MAX=4 # max number of times a job can be submitted to the cluster
-BATCH_SIZE_EXIT=64 # use a smaller batch size for previously failed jobs
+BATCH_SIZE_EXIT=32 # use a smaller batch size for previously failed jobs
 FILE_NAME_PARAMS="params.csv"
 FILE_NAME_JOB="rnn.lsf"
 FILE_NAME_JOB_TEMPLATE="rnn_template.lsf"
