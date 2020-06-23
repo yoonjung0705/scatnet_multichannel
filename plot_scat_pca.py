6f2f91392445892d836175c4f8adb129598fb34d
Yoon Jung <yoonjung0705@gmail.com>
2020-06-22 23:11:24 -0400

    REF:instead of making the labels for each time series as a string, now using multiple for loops and making the plot label within the loop. Only refactored for two beads case so far

--- plot_scat_pca.py
+++ plot_scat_pca.py
@@ -7,19 +7,20 @@ import sim_utils as siu
 import scat_utils as scu
 import matplotlib.pyplot as plt
 from sklearn import decomposition
-from mpl_toolkits.mplot3d import Axes3D
+#from mpl_toolkits.mplot3d import Axes3D
 
-plt.style.use('dark_background')
+#plt.style.use('dark_background')
 fontsize_title = 18
 fontsize_label = 14
-
+fontsize_legend = 14
 # scatnet parameters
 data_len = 2**11
 avg_len = 2**8
-n_data = 200
+n_data = 20
 dt = 0.001
 n_filter_octave = [1,1]
 sim_type = 'tbd' # 'brw', 'psn', 'obd', 'tbd'
+marker_size_mean = 13
 
 n_decim = 3 # precision for external parameters
 scat = scu.ScatNet(data_len, avg_len, n_filter_octave=n_filter_octave)
@@ -96,10 +97,12 @@ elif sim_type == 'obd':
 
 # simulation two beads
 elif sim_type == 'tbd':
-    gammas = 1
+    gammas = np.arange(1,1.4,0.05) # in paper it's [1, 3]
     #k_ratios = np.arange(2,9,2)
-    k_ratios = np.arange(2,3)
-    diff_coef_ratios = np.arange(3,19,2)
+    #k_ratios = [1,3,5,7]
+    k_ratios = [4] # in paper it's [1,7]
+    #diff_coef_ratios = np.arange(4,11) # in paper it's [4,10]
+    diff_coef_ratios = [4,6,8,10] 
     #diff_coef_ratios = np.arange(3,4)
     samples = siu.sim_two_beads(data_len, gammas=gammas, k_ratios=k_ratios, diff_coef_ratios=diff_coef_ratios, dt=dt, n_data=n_data) 
     traj_tbd = samples['data']
@@ -110,28 +113,57 @@ elif sim_type == 'tbd':
     S_tbd_log_mean = S_tbd_log.mean(axis=-1)
     S_tbd_log_mean = np.reshape(S_tbd_log_mean, (S_tbd_log_mean.shape[0], -1))
 
-    diff_coef_ratios_mesh, k_ratios_mesh = np.meshgrid(diff_coef_ratios, k_ratios, indexing='ij')
+    gammas_mesh, diff_coef_ratios_mesh, k_ratios_mesh = np.meshgrid(gammas, diff_coef_ratios, k_ratios, indexing='ij')
 
-    diff_coef_ratios_str = np.round(diff_coef_ratios_mesh, n_decim).astype(str)
-    labels_diff_coef_ratios = np.repeat(np.expand_dims(diff_coef_ratios_str, axis=-1), n_data, axis=-1)
-    labels_diff_coef_ratios = np.char.add('D=', labels_diff_coef_ratios)
+    #gammas_str = np.round(gammas_mesh, n_decim).astype(str)
+    labels_gammas = np.repeat(np.expand_dims(gammas_mesh, axis=-1), n_data, axis=-1).flatten()
+    labels_diff_coef_ratios = np.repeat(np.expand_dims(diff_coef_ratios_mesh, axis=-1), n_data, axis=-1).flatten()
+    labels_k_ratios = np.repeat(np.expand_dims(k_ratios_mesh, axis=-1), n_data, axis=-1).flatten()
+    #labels_gammas = np.char.add('\gamma=', labels_gammas)
+    marker_sizes = labels_gammas**2.75
+    marker_sizes = marker_sizes / np.mean(marker_sizes) * marker_size_mean
 
-    k_ratios_str = np.round(k_ratios_mesh, n_decim).astype(str)
-    labels_k_ratios = np.repeat(np.expand_dims(k_ratios_str, axis=-1), n_data, axis=-1)
-    labels_k_ratios = np.char.add(', k=', labels_k_ratios)
+    #diff_coef_ratios_str = np.round(diff_coef_ratios_mesh, n_decim).astype(str)
+    #labels_diff_coef_ratios = np.repeat(np.expand_dims(diff_coef_ratios_str, axis=-1), n_data, axis=-1)
+    #labels_diff_coef_ratios = np.char.add(r'$T_h/T_c=$', labels_diff_coef_ratios)
 
-    labels_tbd = np.char.add(labels_diff_coef_ratios, labels_k_ratios)
+    #k_ratios_str = np.round(k_ratios_mesh, n_decim).astype(str)
+    #labels_k_ratios = np.repeat(np.expand_dims(k_ratios_str, axis=-1), n_data, axis=-1)
+    #labels_k_ratios = np.char.add(r'$, k=$', labels_k_ratios)
+
+    #labels_tbd = np.char.add(labels_diff_coef_ratios, labels_k_ratios)
     #labels_tbd = np.char.add('obd', labels_tbd)
-    labels_tbd = labels_tbd.flatten()
+    #labels_tbd = labels_tbd.flatten()
 
     X = S_tbd_log_mean
-    labels = labels_tbd
+    #labels = labels_tbd
+
+    #labels_uniq = np.unique(labels)
+
+    pca = decomposition.PCA(n_components=2)
+    pca.fit(X)
+    X = pca.transform(X)
+
+    fig, ax = plt.subplots()
+    for diff_coef_ratio in diff_coef_ratios:
+        for k_ratio in k_ratios:
+            #label = r'$T_h/T_c={}, k_2/k_1={}$'.format(diff_coef_ratio, k_ratio)
+            label = r'$T_h/T_c={}$'.format(diff_coef_ratio)
+            filt = labels_diff_coef_ratios == diff_coef_ratio
+            filt &= labels_k_ratios == k_ratio
+            ax.scatter(X[filt, 0], X[filt, 1], label=label, s=marker_sizes[filt])
+
+    ax.legend(fontsize=fontsize_legend)
+    ax.set_title('PCA after scattering transform', fontsize=fontsize_title)
+    ax.set_xlabel('PC1', fontsize=fontsize_label)
+    ax.set_ylabel('PC2', fontsize=fontsize_label)
+
+    plt.show()
 
 else:
     raise("sim_type should be either 'brw', 'psn', 'obd', 'tbd'")
-
+"""
 labels_uniq = np.unique(labels)
-y = labels
 
 pca = decomposition.PCA(n_components=2)
 pca.fit(X)
@@ -140,7 +172,7 @@ X = pca.transform(X)
 fig,ax = plt.subplots()
 for label in labels_uniq:
     X_label = X[label==labels, :]
-    ax.scatter(X_label[:, 0], X_label[:, 1], s=6, label=label)
+    ax.scatter(X_label[:, 0], X_label[:, 1], label=label, s=marker_sizes[label==labels])
 
 ax.legend()
 ax.set_title('PCA after scattering transform', fontsize=fontsize_title)
@@ -148,4 +180,4 @@ ax.set_xlabel('PC1', fontsize=fontsize_label)
 ax.set_ylabel('PC2', fontsize=fontsize_label)
 
 plt.show()
-
+"""
